310f694d47e50b9b0f74e07a70cad67cacc24151
Oleksandr-Pik <pik.aleksandr79@gmail.com>
2023-10-14 22:06:21 +0300

    task 1,3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,38 @@
 console.log('Hello ');
+
+// Завдання 1 - перемикач кольорів
+
+// HTML містить кнопки «Start» і «Stop».
+
+// <button type="button" data-start>Start</button>
+// <button type="button" data-stop>Stop</button>
+
+// Напиши скрипт, який після натискання кнопки «Start», раз на секунду змінює колір фону <body> на випадкове значення, використовуючи інлайн стиль. Натисканням на кнопку «Stop» зміна кольору фону повинна зупинятися.
+
+// УВАГА
+// Враховуй, що на кнопку «Start» можна натиснути нескінченну кількість разів. Зроби так, щоб доки зміна теми запущена, кнопка «Start» була неактивною (disabled).
+
+// Для генерування випадкового кольору використовуй функцію getRandomHexColor.
+
+
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}
+
+let intervalId = null;
+
+startButton.addEventListener('click', () => {
+    intervalId = setInterval(() => {
+      document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    startButton.disabled = true;
+});
+
+stopButton.addEventListener('click', () => {
+    clearInterval(intervalId);
+    intervalId = null;
+    startButton.disabled = false;
+});
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,55 @@
+// Завдання 3 - генератор промісів
+
+// HTML містить розмітку форми, в поля якої користувач буде вводити першу затримку в мілісекундах, крок збільшення затримки для кожного промісу після першого і кількість промісів, яку необхідно створити.
+
+// Напиши скрипт, який на момент сабміту форми викликає функцію createPromise(position, delay) стільки разів, скільки ввели в поле amount. Під час кожного виклику передай їй номер промісу (position), що створюється, і затримку, враховуючи першу затримку (delay), введену користувачем, і крок (step).
+
+// Доповни код функції createPromise таким чином, щоб вона повертала один проміс, який виконується або відхиляється через delay часу. Значенням промісу повинен бути об'єкт, в якому будуть властивості position і delay зі значеннями однойменних параметрів. Використовуй початковий код функції для вибору того, що потрібно зробити з промісом - виконати або відхилити.
+
+// Бібліотека повідомлень
+
+// Для відображення повідомлень користувачеві, замість console.log(), використовуй бібліотеку notiflix.
+
+// Підключення бібліотеки Notiflix
+import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const firstDelay = Number(formData.get('delay'));
+  const step = Number(formData.get('step'));
+  const amount = Number(formData.get('amount'));
+
+  createPromises(firstDelay, step, amount);
+});
+
+function createPromises(firstDelay, step, amount) {
+  for (let i = 1; i <= amount; i += 1) {
+    const currentDelay = firstDelay + (i - 1) * step;
+    createPromise(i, currentDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
 }
