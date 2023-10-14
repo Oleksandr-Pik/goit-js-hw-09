9f5da6ada8f18d8af9c565c8088473cb37178147
Oleksandr-Pik <pik.aleksandr79@gmail.com>
2023-10-14 22:15:11 +0300

    fix task - 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -18,6 +18,8 @@ console.log('Hello ');
 const startButton = document.querySelector('[data-start]');
 const stopButton = document.querySelector('[data-stop]');
 
+stopButton.disabled = true;
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
 }
